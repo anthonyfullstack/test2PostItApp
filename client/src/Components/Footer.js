dbfc82eba64a64d81018e6f8a82dfd01016554bb
anthonyfullstack <anthony.madlambayan@utas.edu.om>
2024-12-04 14:47:19 +0400

    Update Footer.js - 2nd update here

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>©2023.PostIT. All Rights Reserved. - my first update here</div>
+      <div>©2023.PostIT. All Rights Reserved. - another update here</div>
     </footer>
   );
 };
